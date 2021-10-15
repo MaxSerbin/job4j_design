d1a4f552a77be854ca5774602a51297b770b407a
MaxSerbin <silverspoor644@gmail.com>
2021-10-15 17:48:18 +0300

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> result = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))){
+            String line;
+            while ((line = in.readLine()) != null) {
+                if(line.contains("404")) {
+                    result.add(line);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+    }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}
