d5de93e5fa435d21995d9c70bdd61499587857b5
MaxSerbin <silverspoor644@gmail.com>
2021-10-29 20:14:24 +0300

    4.2. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -8,6 +8,7 @@ public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         if (args.length > 0) {
             Files.walkFileTree(Path.of(args[0]), new DuplicatesVisitor());
+            DuplicatesVisitor.getDuplicates();
         }
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,16 +5,25 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private final HashSet<FileProperty> duplicates = new HashSet<>();
+    private final HashSet<FileProperty> check = new HashSet<>();
+    private static final List<FileProperty> duplicates = new ArrayList<>();
+
+    public static void getDuplicates() {
+        for (FileProperty duplicate : duplicates) {
+            System.out.println("Duplicate : " + duplicate);
+        }
+    }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProp = new FileProperty(file.toFile().length(), file.toFile().getName());
-        if (duplicates.contains(fileProp)) {
-            System.out.println("Duplicate : " + file.toAbsolutePath());
+        if (!check.contains(fileProp)) {
+             check.add(fileProp);
         } else {
             duplicates.add(fileProp);
         }
