10217beb5b99e1fa469a486198cf9d39737a37e8
MaxSerbin <silverspoor644@gmail.com>
2021-11-11 19:30:29 +0300

    5.1. Именованные аргументы [#295518 #224564]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -5,31 +5,28 @@ import java.util.Map;
 
 public class ArgsName {
     private final Map<String, String> values = new HashMap<>();
-    private String[] rsl;
 
     public String get(String key) {
         return values.get(key);
     }
 
-    public boolean check(String[] args) {
+    public void check(String[] args) {
         if (args.length == 0) {
             throw new IllegalArgumentException("args is null");
         }
         for (String i : args) {
-             rsl = i.split("=");
+            String[] rsl = i.split("=");
             if (rsl.length != 2 || rsl[0].isEmpty() || !rsl[0].startsWith("-")) {
                 throw new IllegalArgumentException("Ошибка ! Нарушение шаблона ключ=значение.");
             }
         }
-       return false;
     }
 
     private void parse(String[] args) {
-            if (!check(args)) {
-                for (String i : args) {
-                    rsl = i.split("=");
-                values.put(rsl[0].substring(1), rsl[1]);
-            }
+        check(args);
+        for (String i : args) {
+            String[] rsl = i.split("=");
+            values.put(rsl[0].substring(1), rsl[1]);
         }
     }
 
