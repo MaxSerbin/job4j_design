8d4c877a359ac02f462f4442966bb1aa9dcf05de
MaxSerbin <silverspoor644@gmail.com>
2022-03-23 18:11:11 +0300

    0. JDBC [#6863 #279052]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -9,9 +9,9 @@ import ru.job4j.io.ConfigJdbc;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        ConfigJdbc config = new ConfigJdbc("C:\\projects\\job4j_design\\data\\app.properties");
+        ConfigJdbc config = new ConfigJdbc("./data/app.properties");
         config.load();
+        Class.forName(config.value("driver"));
         try (Connection connection = DriverManager
                 .getConnection(config.value("url"), config.value("login"), config.value("password"))) {
             DatabaseMetaData metaData = connection.getMetaData();
