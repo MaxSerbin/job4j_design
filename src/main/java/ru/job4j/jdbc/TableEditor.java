dd1d6801680516862052a31a17573758f61334b3
MaxSerbin <silverspoor644@gmail.com>
2022-03-24 21:54:16 +0300

    0.1. Statement [#379306 #279444]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -1,6 +1,5 @@
 package ru.job4j.jdbc;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.sql.*;
 import java.util.Properties;
@@ -20,14 +19,6 @@ public class TableEditor implements AutoCloseable {
             connection = DriverManager
                     .getConnection(properties.getProperty("url"), properties.getProperty("login"),
                             properties.getProperty("password"));
-
- //       try (FileInputStream f = new FileInputStream("./data/table_editor.properties")) {
-//            properties.load(f);
-//            Class.forName(properties.getProperty("driver"));
-//            connection = DriverManager
-//                    .getConnection(properties.getProperty("url"), properties.getProperty("login"),
-//                            properties.getProperty("password"));
-//        }
     }
 
     private void doIt(String script) throws SQLException {
